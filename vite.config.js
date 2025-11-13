335987a33338eb16da4ceeebf9dde44748216220
MartinAndresMDQ <martinandres987@gmail.com>
2025-11-13 10:38:08 -0300

    update main

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pokemon-tcg-comparator/', // Reemplaza con el nombre de tu repositorio
+  base: '/Pokemon-TCG-Compare/', // Reemplaza con el nombre de tu repositorio
   plugins: [react()],
   build: {
     outDir: 'dist', // Directorio de salida para el build
